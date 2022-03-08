cf794deda301a85c5ff1399c8a3eda188d1b3d20
joarant <joakimran@gmail.com>
2022-03-08 22:07:09 +0200

    add ship, board and inventory components

--- src/App.js
+++ src/App.js
@@ -12,14 +12,8 @@ function App() {
           <code>src/App.js</code>
           and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        Learn React
       </header>
     </div>
   );
---
+++ src/components/Ship.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Ship() {
+  const a = 1;
+
+  return (
+    <p>alus</p>
+  );
+}
+export default Ship;

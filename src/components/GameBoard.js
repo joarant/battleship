cdb5991581a5d1d95a8d423aafaaef7e7fb70766
joarant <joakimran@gmail.com>
2022-03-11 13:41:21 +0200

    add dragging to gameboard

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Grid, Paper, Button,
 } from '@material-ui/core';
+import Ship from './Ship';
 
 // <img src="your.svg#svgView(preserveAspectRatio(none))" />
 function GameBoard() {
   const boardA = [];
-  const boardB = [];
 
   for (let index = 0; index < 8; index += 1) {
     boardA.push([]);
@@ -14,36 +14,56 @@ function GameBoard() {
       boardA[index].push(`${index}${a}`);
     }
   }
+  useEffect(() => {
+    // code to run after render goes here
+    const myPics = document.getElementById('gridi');
+    myPics.addEventListener('mouseup', (e) => {
+      const grid = e.target;
+      console.log(grid);
+    });
+  }, []);
+
+  const allowDrop = (ev) => {
+    ev.preventDefault();
+  };
+
+  const drop = (ev) => {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData('text');
+    ev.target.appendChild(document.getElementById(data));
+  };
 
   return (
-    <Grid item xs={12}>
+    <>
+      <Ship aax="dadsa" />
+      <Ship aax="da2dsa" />
 
-      {boardA.map((row) => (
-        <Grid container justifyContent="center" spacing={0}>
-          {row.map((value) => (
-            <Grid key={value} item justifyContent="center">
-              <Grid
-                sx={{
-                  height: 100,
-                  width: 100,
-                  opacity: 0.5,
-                  backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#1A2027' : '#ffffff'),
-                }}
-              >
-                <img
-                  src="images/arrow.svg"
-                  alt=""
-                  // width={100}
-                  // height=
-                  style={{ height: '100%', width: '300%' }}
+      <Grid item xs={12} id="gridi">
 
-                />
+        {boardA.map((row) => (
+          <Grid container justifyContent="center" spacing={0}>
+            {row.map((value) => (
+              <Grid key={value} item justifyContent="center">
+                <Paper
+                  sx={{
+                    height: 100,
+                    width: 100,
+                    // opacity: 0.5,
+                    backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#1A2027' : '#ffffff'),
+                  }}
+                  style={{ alignContent: 'center' }}
+                  variant="outlined"
+                  onDrop={drop}
+                  onDragOver={allowDrop}
+                >
+                  {/* <Ship aax={value} /> */}
+                </Paper>
               </Grid>
-            </Grid>
-          ))}
-        </Grid>
-      ))}
-    </Grid>
+            ))}
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
 export default GameBoard;
--- src/components/Ship.js
+++ src/components/Ship.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
-function Ship({ sprite, size }) {
+function Ship({
+  sprite, size, beingMoved, aax,
+}) {
   // const a = 1;
+  function drag(ev) {
+    ev.dataTransfer.setData('text', ev.target.id);
+  }
 
   return (
-    <p>alus</p>
+
+    <img
+      id={aax}
+      src="images/boksi.svg"
+      alt=""
+      width={120}
+      height={100}
+      style={{ position: 'absolute', alignContent: 'center' }}
+      onDragStart={drag}
+    />
   );
 }
 export default Ship;

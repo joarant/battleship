d429bafc2d05d92246ccb423bd48ebb092b1a8ad
joarant <joakimran@gmail.com>
2022-03-14 00:31:47 +0200

    add proper element dragging

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -4,6 +4,7 @@ import {
 } from '@material-ui/core';
 import Ship from './Ship';
 
+// https://www.w3schools.com/howto/howto_js_draggable.asp
 function GameBoard(info) {
   const boardA = [];
   console.log(info.info, info.info.y);
@@ -18,15 +19,6 @@ function GameBoard(info) {
     }
   }
 
-  useEffect(() => {
-    // code to run after render goes here
-    const myPics = document.getElementById('gridi');
-    myPics.addEventListener('mouseup', (e) => {
-      const grid = e.target;
-      console.log(grid);
-    });
-  }, []);
-
   const allowDrop = (ev) => {
     ev.preventDefault();
   };
@@ -34,13 +26,77 @@ function GameBoard(info) {
   const drop = (ev) => {
     ev.preventDefault();
     const data = ev.dataTransfer.getData('text');
-    ev.target.appendChild(document.getElementById(data));
+    const draggable = document.getElementById(data);
+    // console.log(draggable.style.left);
+    // console.log(ev.target.style.left);
+    // draggable.style.left = 0;
+    // draggable.style.top = 0;
+
+    const rect = ev.target.getBoundingClientRect();
+    console.log(rect);
+
+    // console.log(ev.target);
+    // draggable.style.transform = 'rotate(180deg)';
+    draggable.style.left = `${rect.left}px`;
+    draggable.style.top = `${rect.top}px`;
+    // console.log(draggable.getBoundingClientRect());
+
+    // ev.target.appendChild(document.getElementById(data));
   };
 
+  function dragElement(elmnt) {
+    const dragEle = elmnt;
+    let pos1 = 0; let pos2 = 0; let pos3 = 0; let
+      pos4 = 0;
+
+    function elementDrag(e) {
+      let eve = e;
+      eve = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      dragEle.style.top = `${elmnt.offsetTop - pos2}px`;
+      dragEle.style.left = `${elmnt.offsetLeft - pos1}px`;
+    }
+
+    function closeDragElement() {
+      /* stop moving when mouse button is released: */
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+
+    function dragMouseDown(e) {
+      let eve = e;
+      eve = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+
+    dragEle.onmousedown = dragMouseDown;
+  }
+
+  useEffect(() => {
+    // code to run after render goes here
+    dragElement(document.getElementById('da2dsa'));
+
+    const myPics = document.getElementById('gridi');
+    myPics.addEventListener('mouseup', (e) => {
+      const grid = e.target;
+      console.log(grid);
+    });
+  }, []);
   return (
     <>
-      <Ship aax="dadsa" />
-      <Ship aax="da2dsa" />
+      <Ship imgId="da2dsa" />
 
       <Grid item xs={12} id="gridi">
 
@@ -50,8 +106,8 @@ function GameBoard(info) {
               <Grid key={value} item justifyContent="center">
                 <Paper
                   sx={{
-                    height: 100,
-                    width: 100,
+                    height: 80,
+                    width: 80,
                     // opacity: 0.5,
                     backgroundColor: (theme) => (theme.palette.mode === 'dark' ? '#1A2027' : '#ffffff'),
                   }}
--- src/components/Ship.js
+++ src/components/Ship.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Ship({
-  sprite, size, beingMoved, aax,
+  sprite, size, beingMoved, imgId, parentCellPosition,
 }) {
   // const a = 1;
   const drag = (ev) => {
     ev.dataTransfer.setData('text', ev.target.id);
   };
 
+  useEffect(() => {
+    const myPics = document.getElementById(imgId);
+    window.addEventListener('keyup', (e) => {
+      console.log('moi');
+      if (myPics.style.transform === 'rotate(270deg)') {
+        myPics.style.transform = 'rotate(360deg)';
+      } else {
+        myPics.style.transform = 'rotate(270deg)';
+      }
+      console.log(myPics.getBoundingClientRect());
+    });
+  }, []);
+
   return (
 
     <img
-      id={aax}
-      src="images/boksi.svg"
+      id={imgId}
+      src="images/arrow.svg"
       alt=""
       width={120}
       height={100}
-      style={{ position: 'absolute', alignContent: 'center' }}
+      style={{ position: 'absolute' }}
       onDragStart={drag}
     />
   );
